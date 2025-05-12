4aefa3537ceeb14953f67ac10af683e273d8c859
lucash <lucasAdmin@gmail.com>
2025-05-12 13:54:37 -0300

    test: Implementa relógio dinâmico com lifecycle methods

--- src/App.js
+++ src/App.js
@@ -4,35 +4,22 @@ class App extends Component{
 
   constructor(props){
     super(props);
-    this.state = {
-      nome: "Matheus", 
-      contador: 0
+    this.state = { 
+      hora: '00:00:00'
     };
-
-    this.aumentar = this.aumentar.bind(this);
-    this.diminuir = this.diminuir.bind(this);
   }
 
-  aumentar(){
-    let state = this.state;
-    state.contador += 1;
-    this.setState(state)
-  }
+  componentDidMount(){
 
-  diminuir(){
-    let state = this.state;
-    state.contador -= 1;
-    this.setState(state)
+    setInterval(() =>{
+        this.setState({hora: new Date().toLocaleTimeString()})
+    }, 2000);
   }
 
   render(){
     return(
       <div>
-        <h1>Contador</h1>
-        <h3><button onClick={this.diminuir}>-</button>
-          {this.state.contador}
-        <button onClick={this.aumentar}>+</button>
-        </h3>
+        <h1>Meu projeto {this.state.hora}</h1>
       </div>
     );
   }
