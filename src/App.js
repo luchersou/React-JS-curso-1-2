5f586275c7a141c37f9f9017e33d885273fbad77
lucash <lucasAdmin@gmail.com>
2025-05-12 15:12:05 -0300

    test: Implementa controle de autenticação via estado no componente App
    Adiciona lógica condicional baseada no estado do componente para alternar entre mensagens de boas-vindas e prompt de login. Utiliza métodos de classe para alterar o estado e renderizar dinamicamente a interface de acordo com o status do usuário.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,37 @@ import Membro from './components/Membro';
 
 class App extends Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+        status: false
+    };
+
+    this.sair = this.sair.bind(this);
+    this.entrar = this.entrar.bind(this);
+  }
+
+  sair(){
+    this.setState({status: false});
+  }
+
+  entrar(){
+    this.setState({status: true});
+  }
+
   render(){
     return(
       <div>
-        <Membro nome="visitante"/>
+        {this.state.status ?
+        <div>
+          <h2>Bem vindo ao sistema</h2>
+          <button onClick={this.sair}>Sair</button>
+        </div> :
+        <div>
+          <h2>Olá visitante, faça o login</h2>
+          <button onClick={this.entrar}>Entrar no sistema</button>
+        </div>
+        }
       </div>
     );
   }
