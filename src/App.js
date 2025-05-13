91a1c3e72897d6c1205a44c473f74d914e938772
lucash <lucasAdmin@gmail.com>
2025-05-13 13:54:49 -0300

    test: Implementa componente Feed com renderização condicional de curtidas e comentários
    Adiciona o componente Feed que recebe props de um array de objetos contendo informações
    
    de usuários, como curtidas e comentários. O texto é exibido de forma condicional:
    
    - Exibe curtida no singular quando a quantidade é 1, e curtidas no plural caso contrário.
    
    - Exibe comentarios apenas se o número for maior que 1.
    
    A renderização é feita dinamicamente com base no estado do componente App.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,29 @@
 import React, { Component } from 'react';
-import Membro from './components/Membro';
+import Feed from './components/Feed';
 
 class App extends Component{
 
   constructor(props){
     super(props);
     this.state = {
-        status: false
+        feed: [
+          {id: 1, username: 'Lucas', curtidas: 10, comentarios: 2},
+          {id: 2, username: 'Matheus', curtidas: 5, comentarios: 24},
+          {id: 3, username: 'Amanda', curtidas: 15, comentarios: 7},
+          {id: 4, username: 'Ricardo', curtidas: 1, comentarios: 0},
+        ]
     };
-
-    this.sair = this.sair.bind(this);
-    this.entrar = this.entrar.bind(this);
-  }
-
-  sair(){
-    this.setState({status: false});
-  }
-
-  entrar(){
-    this.setState({status: true});
   }
 
   render(){
     return(
-      <div>
-        {this.state.status ?
-        <div>
-          <h2>Bem vindo ao sistema</h2>
-          <button onClick={this.sair}>Sair</button>
-        </div> :
-        <div>
-          <h2>Olá visitante, faça o login</h2>
-          <button onClick={this.entrar}>Entrar no sistema</button>
-        </div>
-        }
+      <div>        
+        {this.state.feed.map((item) => {
+          return(            
+            <Feed id={item.id} username={item.username} curtidas={item.curtidas} comentarios={item.comentarios} />
+          );
+        })}
+
       </div>
     );
   }
